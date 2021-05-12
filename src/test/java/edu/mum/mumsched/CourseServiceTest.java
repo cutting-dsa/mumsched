f1634c5aea7da90df14d929db9329b10c446358f
Quest <b.quest@hotmai.com>
2021-05-12 11:10:41 -0500

    course test

---
+++ src/test/java/edu/mum/mumsched/CourseServiceTest.java
@@ -0,0 +1,28 @@
+package edu.mum.mumsched;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseServiceTest {
+
+    @Test
+    void getAllCourses() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void updateCourse() {
+    }
+
+    @Test
+    void deleteCourse() {
+    }
+
+    @Test
+    void getCourse() {
+    }
+}

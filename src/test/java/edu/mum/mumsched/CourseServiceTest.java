145ed8fec7089d54a65df5b776a36b90d3ee18ea
Quest <b.quest@hotmai.com>
2021-05-13 22:48:05 -0500

    Update CourseServiceTest.java

--- src/test/java/edu/mum/mumsched/CourseServiceTest.java
+++ src/test/java/edu/mum/mumsched/CourseServiceTest.java
@@ -1,13 +1,26 @@
 package edu.mum.mumsched;
 
+import edu.mum.mumsched.courses.service.CourseService;
+import edu.mum.mumsched.courses.service.impl.CourseServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
+@RunWith(MockitoJUnitRunner.class)
 class CourseServiceTest {
+    @Mock
+    private CourseService courseServiceMock;
 
+    @InjectMocks
+    private CourseServiceImpl courseServiceImpl;
     @Test
     void getAllCourses() {
+        CourseServiceImpl
     }
 
     @Test
